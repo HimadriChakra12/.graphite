7566c435fc327f67c789867569345efcd2d319a0
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-08-18 04:52:46 -0700

    Graphite [nvim : plugs]

--- nvim/lua/plugs.lua
+++ nvim/lua/plugs.lua
@@ -10,17 +10,6 @@ return {
         end
     },
     {
-        name = "buffer-manager",
-        url = "https://github.com/xsoder/buffer-manager.nvim.git",
-        dependencies = {
-            { url = "https://github.com/nvim-tree/nvim-web-devicons" },
-            { url = "https://github.com/ibhagwan/fzf-lua" },
-        },
-        config = function()
-            require("buffer-manager").setup()
-        end,
-    },
-    {
         name = "Undotree",
         url = "https://github.com/mbbill/undotree",
         config = function()
