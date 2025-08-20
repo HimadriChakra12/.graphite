a22c31d82581c6bcb0ec9526fed288bad6b7ac4b
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-08-20 05:04:56 -0700

    Graphite [reg]

---
+++ Reg/.reg/create_download.ps1
@@ -0,0 +1,8 @@
+param([string]$folderPath)
+
+$today = Get-Date -Format "yyyy-MM-dd"
+$newFolder = Join-Path -Path $folderPath -ChildPath "Downloads $today"
+
+if (-Not (Test-Path $newFolder)) {
+    New-Item -ItemType Directory -Path $newFolder | Out-Null
+}
